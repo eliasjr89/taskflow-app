be85569e22bd7856022ff5a8ef479ce980b7edb3
eliasjr89 <eliasjiminian@gmail.com>
2026-01-07 20:14:09 +0100

    feat: Add PWA support by implementing a service worker for caching and a web app manifest.

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import App from "./App.vue";
 import router from "./router";
 import "./global.css";
 import i18n from "./i18n";
+import "./registerSW";
 
 const app = createApp(App);
 const pinia = createPinia();
